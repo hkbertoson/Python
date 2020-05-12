e9aa0d56fc88789ba9d746947619166384327e92
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2020-05-12 10:59:05 -0500

    Added Files.

---
+++ Project 3.py
@@ -0,0 +1,11 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Your name is" ,name)
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    print("Your name is" ,name)
+NameError: name 'name' is not defined
+>>> name = 'Hunter Bertoson'
+>>> print("Your name is" ,name)
+Your name is Hunter Bertoson
+>>> 
